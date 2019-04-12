03f3b42802f8147f49c84fe22b7a457b967b950c
Samuel Marks <samuelmarks@gmail.com>
2019-04-12 16:24:55 +1000

    Adding a bunch of gpg keys and bintray, also generate_temp_password => gen_random_str from offutils

--- offregister_rabbitmq/ubuntu/__init__.py
+++ offregister_rabbitmq/ubuntu/__init__.py
@@ -1,13 +1,23 @@
+from fabric.contrib.files import append
 from fabric.operations import sudo, run
 
 from offregister_fab_utils.apt import apt_depends
-from offutils import generate_temp_password
+from offutils import gen_random_str
 
 
 def install0(**kwargs):
     installed = lambda: run("dpkg-query --showformat='${Version}' --show rabbitmq-server", quiet=True)
 
     if sudo('dpkg -s rabbitmq-server', quiet=True, warn_only=True).failed:
+        sudo('wget -O - "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | apt-key add -')
+        sudo('apt-key adv --keyserver "hkps.pool.sks-keyservers.net" --recv-keys "0x6B73A36E6026DFCA"')
+        sudo(
+            'wget -O - "https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc" | apt-key add -')
+        append('/etc/apt/sources.list.d/bintray.rabbitmq.list',
+               'deb https://dl.bintray.com/rabbitmq/debian {codename} main'.format(
+                   codename=run('lsb_release -cs', quiet=True)
+               ),
+               use_sudo=True)
         apt_depends('rabbitmq-server')
         return 'RabbitMQ {} installed'.format(installed())
 
@@ -15,7 +25,7 @@ def install0(**kwargs):
 
 
 def create_user1(**kwargs):
-    password = kwargs.get('password', generate_temp_password(15))
+    password = kwargs.get('password', gen_random_str(15))
     sudo("rabbitmqctl add_user {rmq_user} '{password}'".format(rmq_user=kwargs['rmq_user'], password=password),
          shell_escape=False)
     if 'rmq_vhost' in kwargs:
